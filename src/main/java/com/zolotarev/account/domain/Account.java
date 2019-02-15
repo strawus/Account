e8b0f66be56fb61c9ef7d4d716d7fcb66f89208f
strawus <a.zolotarev@cdek.ru>
2019-02-15 17:34:59 +0300

    Entity tuned in jpa style. Added field "version" to optimistic updating

--- src/main/java/com/zolotarev/account/domain/Account.java
+++ src/main/java/com/zolotarev/account/domain/Account.java
@@ -1,39 +1,57 @@
 package com.zolotarev.account.domain;
 
 import com.zolotarev.util.Contract;
-import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.keyvalue.annotation.KeySpace;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.Cache;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
 import static com.zolotarev.constant.Messages.*;
 import static java.math.BigDecimal.ZERO;
+import static javax.persistence.EnumType.STRING;
+import static javax.persistence.GenerationType.SEQUENCE;
+import static lombok.AccessLevel.PRIVATE;
+import static org.hibernate.annotations.CacheConcurrencyStrategy.READ_WRITE;
 
 /**
  * Entity represents basic account operations
  */
-@KeySpace("accounts")
+@Entity
 @Getter
 @ToString
+@DynamicInsert
+@DynamicUpdate
+@NoArgsConstructor(access = PRIVATE)
+@Cache(usage = READ_WRITE, region = "accounts")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public final class Account {
+public final class Account implements Serializable {
 
     //Unique account identificator. If this account didn't stored yet, it can't be used in HashSet/HashMap, because id haven't generated
     @Id
     @EqualsAndHashCode.Include
+    @SequenceGenerator(name = "account_id_seq", allocationSize = 10)
+    @GeneratedValue(strategy = SEQUENCE, generator = "account_id_seq")
     private Integer id;
+
     //Amount held in an account, converted to minor currency
     @Getter(AccessLevel.NONE)
     private BigInteger amountInMinorCurrency;
-    //Currency in which the amount is
-    private final Currency currency;
 
-    public Account(Integer id, BigDecimal amount, Currency currency) {
+    //Currency in which the amount is
+    @Enumerated(STRING)
+    @Column(name = "currency_code", nullable = false)
+    private Currency currency;
+
+    //Entity version. Uses for optimistic updates in database
+    @Version
+    private Integer version;
+
+    public Account(Integer id, BigDecimal amount, Currency currency, Integer version) {
         Contract.requiresNotNull(amount, AMOUNT_MUST_BE_NOT_NULL);
         Contract.requiresNotNull(currency, CURRENCY_MUST_BE_NOT_NULL);
         Contract.requiresMoreOrEquals(amount, ZERO, AMOUNT_MUST_BE_NOT_NEGATIVE);
@@ -41,6 +59,11 @@ public final class Account {
         this.id = id;
         this.amountInMinorCurrency = currency.toMinorCurrency(amount);
         this.currency = currency;
+        this.version = version;
+    }
+
+    public Account(Integer id, BigDecimal amount, Currency currency) {
+        this(id, amount, currency, null);
     }
 
     public Account(BigDecimal amount, Currency currency) {
