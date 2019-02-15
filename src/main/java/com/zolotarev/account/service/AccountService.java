cdae9a0b2c40cd89a04828e93026248a8a6346b4
strawus <a.zolotarev@cdek.ru>
2019-02-15 17:50:46 +0300

    Added transaction management on service layer

--- src/main/java/com/zolotarev/account/service/AccountService.java
+++ src/main/java/com/zolotarev/account/service/AccountService.java
@@ -1,17 +1,16 @@
 package com.zolotarev.account.service;
 
-import com.zolotarev.account.mapper.CopyMachine;
 import com.zolotarev.account.domain.Account;
+import com.zolotarev.account.repository.AccountRepository;
 import com.zolotarev.exception.EntityNotFoundException;
 import com.zolotarev.exception.InternalException;
-import com.zolotarev.account.repository.AccountRepository;
 import com.zolotarev.util.Contract;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,9 +24,7 @@ import static com.zolotarev.constant.Messages.*;
 @RequiredArgsConstructor
 public class AccountService {
 
-
     private final @NonNull AccountRepository repository;
-    private final @NonNull CopyMachine copyMachine;
 
     /**
      * @param accountId Account id to find it. Must be not null
@@ -36,6 +33,7 @@ public class AccountService {
      * @throws EntityNotFoundException  If an entity not found in a storage
      * @throws IllegalArgumentException If method parameters are invalid
      */
+    @Transactional(readOnly = true)
     public Account getById(Integer accountId) {
         Contract.requiresNotNull(accountId, ACCOUNT_ID_MUST_BE_NOT_NULL);
 
@@ -57,13 +55,13 @@ public class AccountService {
      * @throws InternalException        If some error occurs in a storage
      * @throws IllegalArgumentException If method parameters are invalid
      */
+    @Transactional
     public Account create(Account account) {
         Contract.requiresNotNull(account, ACCOUNT_MUST_BE_NOT_NULL);
         Contract.requiresNull(account.getId(), ACCOUNT_ALREADY_EXIST);
 
         try {
-            //Copy-on-update to avoid synchronization problems with thread unsafe entity
-            return repository.save(copyMachine.copy(account));
+            return repository.save(account);
         } catch (RuntimeException e) {
             log.error("Error creating account: {}. Reason[{}]: {}", account, e.getClass(), e.getMessage());
             throw new InternalException("Error creating account[" + account + "]", e);
@@ -78,13 +76,13 @@ public class AccountService {
      * @throws InternalException        If some error occurs in a storage
      * @throws IllegalArgumentException If method parameters are invalid
      */
+    @Transactional
     public Account update(Account account) {
         Contract.requiresNotNull(account, ACCOUNT_MUST_BE_NOT_NULL);
         Contract.requiresNotNull(account.getId(), ACCOUNT_IS_NOT_EXISTS);
 
         try {
-            //Copy-on-update to avoid synchronization problems with thread unsafe entity
-            return repository.save(copyMachine.copy(account));
+            return repository.save(account);
         } catch (RuntimeException e) {
             log.error("Error updating account: {}. Reason[{}]: {}", account, e.getClass(), e.getMessage());
             throw new InternalException("Error updating account[" + account + "]", e);
@@ -97,6 +95,7 @@ public class AccountService {
      * @param accountId Account id to delete. Mustn't be null
      * @throws InternalException If some error occurs in a storage
      */
+    @Transactional
     public void delete(Integer accountId) {
         Contract.requiresNotNull(accountId, ACCOUNT_ID_MUST_BE_NOT_NULL);
 
@@ -112,17 +111,10 @@ public class AccountService {
      * @return All accounts in a storage
      * @throws InternalException If any error occurs in a database
      */
+    @Transactional
     public List<Account> getAll() {
         try {
-            final Iterable<Account> accountIterable = repository.findAll();
-            //Some of implementations CrudRepository#findAll() returns List like Iterable. Casting it to List faster than iteration
-            if (accountIterable instanceof List) {
-                return (List<Account>) accountIterable;
-            } else {
-                final List<Account> accounts = new ArrayList<>();
-                accountIterable.forEach(accounts::add);
-                return accounts;
-            }
+            return repository.findAll();
         } catch (RuntimeException e) {
             log.error("Error getting all accounts. Reason[{}]: {}", e.getClass(), e.getMessage());
             throw new InternalException("Error getting all accounts", e);
