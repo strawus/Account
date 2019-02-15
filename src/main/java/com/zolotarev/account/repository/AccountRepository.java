75010792ed6f97fa2a3ad7d7203763a1eb0485c7
strawus <a.zolotarev@cdek.ru>
2019-02-15 17:31:33 +0300

    Changed repository type from key-value to Jpa

--- src/main/java/com/zolotarev/account/repository/AccountRepository.java
+++ src/main/java/com/zolotarev/account/repository/AccountRepository.java
@@ -1,11 +1,11 @@
 package com.zolotarev.account.repository;
 
 import com.zolotarev.account.domain.Account;
-import org.springframework.data.keyvalue.repository.KeyValueRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
  * Database abstraction layer for an account entity
  */
 @Repository
-public interface AccountRepository extends KeyValueRepository<Account, Integer> { }
+public interface AccountRepository extends JpaRepository<Account, Integer> { }
